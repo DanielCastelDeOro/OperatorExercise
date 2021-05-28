d8fd04a1b428b46de0a320660fb1e68c74a92e5a
Daniel Castel De Oro <daniel.casteldeoro@gmail.com>
2021-05-27 18:53:10 -0600

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+
+            int b = 4;
+
+            int quotient = a / b;
+
+            int remainder = a % b;
+
+            if (a == 17 && b == 4) ;
+
+            //Console.WriteLine(quotient);
+
+            //Console.WriteLine(remainder);
+
+            Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+
+
+        }
+    }
+}
